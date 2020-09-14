7c90e6df45742970b903fe51f3eedf192daa7910
Rimvydas <40775067+RimveBiceps@users.noreply.github.com>
2020-09-14 22:08:43 +0300

    Dimensions, Collision, Unused code
    * Changed map size for fitting all borders
    * Addded exit functionality on pac-man & ghost collision
    * Deleted unused code

--- src/pacman/WindowsMain.java
+++ src/pacman/WindowsMain.java
@@ -2,11 +2,8 @@ package pacman;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,57 +15,56 @@ import javax.swing.WindowConstants;
 
 public class WindowsMain extends JFrame{
 
-	private GameLevel level;
-	private GameRules gameRules;
-	private WindowsRenderer windowsRenderer;
+	private final GameLevel level;
+	private final GameRules gameRules;
+	private final WindowsRenderer windowsRenderer;
 
-	public WindowsMain() throws FileNotFoundException, IOException {
-		super.setPreferredSize(new Dimension(1500, 900));
+	public WindowsMain() throws IOException {
+		super.setPreferredSize(new Dimension(1600, 700));
 		super.pack();
 		super.setVisible(true);
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		
+
 		GameMap map = new GameMap();
 		Pacman pacman = new Pacman( new Position(10,10) );
 		List<Ghost> ghosts = new ArrayList<Ghost>();
 			ghosts.add(new Ghost( new Position(15, 13),  -1, 0));
 			ghosts.add(new Ghost( new Position(17, 10),  0,  1));
-			
+
 		this.level = new GameLevel(map, pacman, ghosts);
-		
+
 		this.gameRules = new GameRules(level);
 
 		this.windowsRenderer = new WindowsRenderer();
-		
+
 		super.addKeyListener(new KeyListener() {
-			
+
 			@Override
 			public void keyTyped(KeyEvent e) {}
-			
+
 			@Override
-			public void keyReleased(KeyEvent e) {}			
-			
+			public void keyReleased(KeyEvent e) {}
+
 			@Override
 			public void keyPressed(KeyEvent e) {
 				char command = e.getKeyChar();
 				repaint();
-				
+
 				gameRules.processUserInput(command);
 			}
 		});
-		
-		new Timer(300, new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				repaint();
-				
-				gameRules.moveGhosts();
-			}
-			
-		}).start();
+
+		Timer timer = new Timer(300, eventListener -> {
+			repaint();
+
+			if(gameRules.isPacmanEaten())
+				System.exit(0);
+
+			gameRules.moveGhosts();
+		});
+		timer.start();
 	}
-	
+
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
@@ -77,19 +73,14 @@ public class WindowsMain extends JFrame{
 	}
 	
 	public static void main(String[] args) throws Exception {
-		SwingUtilities.invokeAndWait(new Runnable() {
-			
-			@Override
-			public void run() {
-				try {
-					new WindowsMain();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+		SwingUtilities.invokeAndWait(() -> {
+			try {
+				new WindowsMain();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		});
 	}
-	
 	
 	private static final long serialVersionUID = 1L;
 
