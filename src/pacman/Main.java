9521353ddba68d26da5450a8087016836c26b9a5
Mantas Urbonas <mantas.urbonas@visma.com>
2020-09-14 17:43:27 +0300

    tuščias projektas

---
+++ src/pacman/Main.java
@@ -0,0 +1,13 @@
+package pacman;
+
+import java.io.IOException;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println("Press any key to continue");
+		System.in.read();
+	}
+
+}
