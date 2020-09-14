061d7255da5663928119cdc3b1a5cd59ec894d7f
Aurimas Šakalys <aurimas.sakalys@stud.vgtu.lt>
2020-09-14 21:17:56 +0300

    FIX: Renderer writing multiple characters fixed.

--- src/pacman/GameRendered.java
+++ src/pacman/GameRendered.java
@@ -2,25 +2,39 @@ package pacman;
 
 public class GameRendered {
 
+	private String toRender = " ";
+	private boolean nonEmptyRender = false;
+
 	public void renderMap(GameLevel gameLevel) {
 		for (int y = 0; y < gameLevel.getGameMap().width(); y++) {
 			for (int x = 0; x < gameLevel.getGameMap().height(); x++) {
+				ResetState();
+
 				for (Ghost ghost: gameLevel.getGhosts()) {
-					if (ghost.at(x, y)){
-						System.out.print("W");
-						continue;
-					}
+					if (ghost.at(x, y))
+						UpdateRender("W");
 				}
 				if (gameLevel.getPacman().getPosition().at(x, y))
-					System.out.print("C");
-				else
-				if (gameLevel.getGameMap().isWall(y, x))
-					System.out.print("#");
-				else
-				if (gameLevel.getGameMap().isAvailable(y, x))
-					System.out.print(" ");
+					UpdateRender("C");
+
+				else if (gameLevel.getGameMap().isWall(y, x))
+					UpdateRender("#");
+
+				System.out.print(toRender);
 			}
 			System.out.println();
+		}
+	}
+
+	private void ResetState() {
+		this.toRender = " ";
+		this.nonEmptyRender = false;
+	}
+
+	private void UpdateRender(String toRender) {
+		if(!this.nonEmptyRender) {
+			this.toRender = toRender;
+			this.nonEmptyRender = true;
 		}
 	}
 }
