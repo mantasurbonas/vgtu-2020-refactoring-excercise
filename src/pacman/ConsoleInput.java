8fe61ec066982d9eb254b991d692fd9cbfabcc46
Aurimas Šakalys <aurimas.sakalys@stud.vgtu.lt>
2020-09-15 20:32:59 +0300

    FORMATTING: Little cleanup.

--- src/pacman/ConsoleInput.java
+++ src/pacman/ConsoleInput.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 public class ConsoleInput {
 
 	public int readConsoleInput() throws IOException {
-		int key = 0;
+		int key;
 		do {
 			key = System.in.read();
-		}while(key == 13 || key ==10);
+		} while(key == 13 || key ==10);
 		return key;
 	}
 }
