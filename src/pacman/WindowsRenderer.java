14afa312b592f561a05f369eb0c08005a659df77
Rimvydas <40775067+RimveBiceps@users.noreply.github.com>
2020-09-21 00:13:55 +0300

    changed size of borders to fit in window

--- src/pacman/WindowsRenderer.java
+++ src/pacman/WindowsRenderer.java
@@ -9,29 +9,28 @@ import javax.imageio.ImageIO;
 
 public class WindowsRenderer {
 
-	private final BufferedImage ghostImage;
-	private final BufferedImage wallImage;
-	private final BufferedImage pacmanImage;
+    private final BufferedImage ghostImage;
+    private final BufferedImage wallImage;
+    private final BufferedImage pacmanImage;
 
-	public WindowsRenderer() throws IOException {
-		pacmanImage = ImageIO.read(new FileInputStream("img/pacman-open.png"));
-		ghostImage = ImageIO.read(new FileInputStream("img/ghost.png"));
-		wallImage = ImageIO.read(new FileInputStream("img/wall.png"));
-	}
+    public WindowsRenderer() throws IOException {
+        pacmanImage = ImageIO.read(new FileInputStream("img/pacman-open.png"));
+        ghostImage = ImageIO.read(new FileInputStream("img/ghost.png"));
+        wallImage = ImageIO.read(new FileInputStream("img/wall.png"));
+    }
 
-	public void render(Graphics g, GameLevel level) {
-		g.drawImage(pacmanImage, 50 + level.getPacman().getPosition().getX() * 20,
-				50 + level.getPacman().getPosition().getY() * 20, 20, 20, null);
+    public void render(Graphics g, GameLevel level) {
+        g.drawImage(pacmanImage, 30 + level.getPacman().getPosition().getX() * 17,
+                30 + level.getPacman().getPosition().getY() * 17, 17, 17, null);
 
-		for (Ghost ghost : level.getGhosts())
-			g.drawImage(ghostImage, 50 + ghost.getPosition().getX() * 20, 50 + ghost.getPosition().getY() * 20, 20, 20,
-					null);
+        for (Ghost ghost : level.getGhosts())
+            g.drawImage(ghostImage, 30 + ghost.getPosition().getX() * 17, 30 + ghost.getPosition().getY() * 17, 17, 17,
+                    null);
 
-		for (int y = 0; y < level.getGameMap().width(); y++) {
-			for (int x = 0; x < level.getGameMap().height(); x++)
-				if (level.getGameMap().isWall(y, x))
-					g.drawImage(wallImage, 50 + x * 20, 50 + y * 20, 20, 20, null);
-		}
-	}
-
+        for (int y = 0; y < level.getGameMap().width(); y++) {
+            for (int x = 0; x < level.getGameMap().height(); x++)
+                if (level.getGameMap().isWall(y, x))
+                    g.drawImage(wallImage, 30 + x * 17, 30 + y * 17, 17, 17, null);
+        }
+    }
 }
